d5af89cbdef3ed612c51e5c9f8802e8e9de74465
masahiro-ta05 <lumdp54931@yahoo.co.jp>
2025-03-10 11:51:58 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+  </head>
+
+  <body>
+    <p>
+       <?php
+       $vegetable_food = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+       print_r($vegetable_food)
+       ?>
+    </P>
+  </body>
+</html>    
